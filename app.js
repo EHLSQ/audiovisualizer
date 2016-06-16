8b77df56ca84aca4044ad2f67f5ff023259b63ab
Evan Li <evan.li.2010@hotmail.com>
2016-06-16 15:40:02 -0400

    Add route for about and navbar

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/', function (req, res) {
   res.sendfile(path.join(__dirname+'/index.html'));
 });
 
+app.get('/navbar.html', function (req, res) {
+  res.sendfile(path.join(__dirname+'/pages/navbar.html'));
+});
+
+app.get('/about.html', function (req, res) {
+  res.sendfile(path.join(__dirname+'/pages/about.html'));
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
