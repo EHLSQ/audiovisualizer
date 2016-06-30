ebce4571c30bdd84d8e33203f990f901ddf7be4f
Evan Li <evan.li.2010@hotmail.com>
2016-06-30 13:58:29 -0400

    Add routes for audio and script file

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ app.get('/about.html', function (req, res) {
   res.sendfile(path.join(__dirname+'/pages/about.html'));
 });
 
+app.get('/audiothree.html', function (req, res) {
+  res.sendfile(path.join(__dirname+'/pages/audiothree.html'));
+});
+
+app.get('/audiofiles/Hide.mp3', function (req, res) {
+  res.sendfile(path.join(__dirname+'/audiofiles/Hide.mp3'));
+});
+
+app.get('/scripts/audiothree.js', function (req, res) {
+  res.sendfile(path.join(__dirname+'/scripts/audiothree.js'));
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
